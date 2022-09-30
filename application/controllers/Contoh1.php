180fc4ab65f2606cab84cee324a2fb69de87c215
panodee <patriciaoppo22@gmail.com>
2022-09-30 23:48:48 +0700

    Tugas Pert3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends C1_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Patricia Novita Devi
+            Saya tinggal di daerah Singkawang
+            Olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
