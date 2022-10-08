b762557920323da4cbbe8db92c385952706e85a9
panodee <patriciaoppo22@gmail.com>
2022-10-08 14:17:21 +0700

    Tugas Pert4

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -7,6 +7,13 @@ class Web extends CI_Controller{
         $this->load->helper('url');
     }
 
+    public function index(){
+        $data['judul'] = "Halaman Index";
+        $this->load->view('v_header',$data);
+        $this->load->view('v_index',$data);
+        $this->load->view('v_footer',$data);
+    }
+
     public function about(){
         $data['judul'] = "Halaman About";
         $this->load->view('v_header',$data);
