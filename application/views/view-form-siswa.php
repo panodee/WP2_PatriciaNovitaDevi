36b7a7fa31e4597dd538671300c6966acb136c24
panodee <patriciaoppo22@gmail.com>
2022-10-21 19:51:41 +0700

    Tugas Pert 6

---
+++ application/views/view-form-siswa.php
@@ -0,0 +1,96 @@
+<html>
+
+<head>
+    <title>Form Input Datasiswa</title>
+</head>
+
+<body>
+    <center>
+        <form   action="<?=     base_url('datasiswa/cetak');   ?>" method="post">
+            <table>
+                <tr>
+                    <th colspan="3")>
+                        Form Input Datasiswa
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama Siswa</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="kode" id="kode">
+                    </td>
+                </tr>
+                <tr>
+                    <th>NIS</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kelas</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Tanggal Lahir</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Tempat Lahir</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Alamat</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Jenis Kelamin</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Agama</th>
+                    <td>:</td>
+                    <td>
+                        <select name="agama" id="agama">
+                            <option value="">Select</option>
+                            <option value="2">Islam</option>
+                            <option value="3">Kristen</option>
+                            <option value="4">Katolik</option>
+                            <option value="5">Protestan</option>
+                            <option value="6">Hindu</option>
+                            <option value="7">Buddha</option>
+                            <option value="8">Khonghucu</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">
+                        <input type="submit" value="Submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
